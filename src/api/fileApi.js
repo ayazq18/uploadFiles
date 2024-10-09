dff2dee16c28fa176dcc827bb21cf494ee3819c2
Ayaz Shahnawaz Qureshi <131868270+ayazq18@users.noreply.github.com>
2024-10-09 10:32:40 +0530

    path updated for updating the type of file

--- src/api/fileApi.js
+++ src/api/fileApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://upload-files-backened.vercel.app",
     withCredentials: false,               // Allow cross-origin cookies for OAuth
   });
 
